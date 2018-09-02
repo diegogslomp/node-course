d7666145723f8fb48cc62de05b4f12211ee8a25f
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-09-02 20:26:24 -0300

    sec 5 lec 63

--- 0558-middleware-functions/index.js
+++ 0558-middleware-functions/index.js
@@ -1,3 +1,4 @@
+const debug = require('debug')('app:startup');
 const config = require('config');
 const helmet = require('helmet');
 const morgan = require('morgan');
@@ -19,7 +20,7 @@ console.log(`Mail Password: ${config.get('mail.password')}`);
 
 if(app.get('env') === 'development') {
   app.use(morgan('tiny'));
-  console.log('Morgan enabled...');
+  debug('Morgan enabled...');
 }
 
 app.use(logger);
