8bedd9436b0441bbc072cd580f5a261ccbadf646
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-09-04 13:45:28 -0300

    sec 8 lec 106

--- 07-085-mongo-demo/index.js
+++ 07-085-mongo-demo/index.js
@@ -46,7 +46,7 @@ async function createCourse() {
   const course = new Course({
     name: 'Angular Course',
     author: 'Diego',
-    category: 'web',
+    category: '-',
     tags: null,
     isPublished: true,
     price: 15
@@ -55,7 +55,8 @@ async function createCourse() {
     const result = await course.save();
     console.log(result);
   } catch(ex) {
-    console.log(ex.message);
+    for (field in ex.errors)
+      console.log(ex.errors[field].message);
   }
 }
 
