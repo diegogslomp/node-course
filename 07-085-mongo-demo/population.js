9949fc6c3c03b6e712abf7f1ab1ab5c035535cdf
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-09-05 10:30:12 -0300

    sec 9 lec 114

--- 07-085-mongo-demo/population.js
+++ 07-085-mongo-demo/population.js
@@ -42,12 +42,13 @@ async function createCourse(name, author) {
 async function listCourses() { 
   const courses = await Course
     .find()
-    .select('name');
+    .populate('author', 'name -_id')
+    .select('name author');
   console.log(courses);
 }
 
 // createAuthor('Mosh', 'My bio', 'My Website');
 
-createCourse('Node Course', '5b8fd79ed0b3b7247f74806a');
+// createCourse('Node Course', '5b8fd79ed0b3b7247f74806a');
 
-// listCourses();
+listCourses();
