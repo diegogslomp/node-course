e4d362cd4a53462b66482777275210d8e9bad8ad
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-09-10 09:27:43 -0300

    sec 9 lec 122

--- 08-108-vidly-with-mongodb/models/movie.js
+++ 08-108-vidly-with-mongodb/models/movie.js
@@ -30,7 +30,7 @@ const Movie = mongoose.model('Movie', mongoose.Schema({
 function validateMovie(movie) {
   const schema = {
     title: Joi.string().min(5).max(255).trim().required(),
-    genreId: Joi.string().required(),
+    genreId: Joi.objectId().required(),
     numberInStock: Joi.number().min(0).max(255),
     dailyRentalRate: Joi.number().min(0).max(255)
   };
