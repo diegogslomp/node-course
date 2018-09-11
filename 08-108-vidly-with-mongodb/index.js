f02ee8e334dd493398692be177493d69b5e325e9
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-09-11 11:52:09 -0300

    sec 10 lec 129

--- 08-108-vidly-with-mongodb/index.js
+++ 08-108-vidly-with-mongodb/index.js
@@ -1,13 +1,14 @@
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
 const customers = require('./routes/customers');
 const genres = require('./routes/genres');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
+const express = require('express');
+const app = express();
 
 mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
   .then(() => console.log('Connected to Mongodb...'))
@@ -19,6 +20,7 @@ app.use('/api/genres', genres);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
