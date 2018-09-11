6cf7e83caf95e818a3713d1af1efcb78b410c368
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-09-11 12:03:16 -0300

    sec 10 lec 130

--- 08-108-vidly-with-mongodb/routes/auth.js
+++ 08-108-vidly-with-mongodb/routes/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const Joi = require('joi');
 const _ = require('lodash');
 const { User } = require('../models/user');
 const express = require('express');
@@ -11,12 +12,11 @@ router.post('/', async (req, res) => {
 
   const user = await User.findOne({ email: req.body.email });
   if(!user) return res.status(400).send('Invalid email or password');
-
+  
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if(!validPassword) return res.status(400).send('Invalid email or password');
 
   res.send(true);
-  
 });
 
 function validate(req) {
@@ -24,7 +24,7 @@ function validate(req) {
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(255).required()
   };
-  return Joi.validate(user, schema);
+  return Joi.validate(req, schema);
 }
 
 module.exports = router;
