5e74032a4db1f862a371c3029c75dd6d29692fe0
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-09-11 09:51:34 -0300

    sec 10 lec 127

--- 08-108-vidly-with-mongodb/routes/users.js
+++ 08-108-vidly-with-mongodb/routes/users.js
@@ -1,23 +1,20 @@
+const _ = require('lodash');
 const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-  
+
   const { error } = validate(req.body);
   if(error) return res.status(400).send(error.details[0].message);
 
   const alreadyExists = await User.findOne({ email: req.body.email });
   if(alreadyExists) return res.status(400).send('The given user email already exists');
 
-  const user = new User({ 
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  
+  const user = new User(_.pick(req.body, ['name', 'email', 'password']));
   await user.save();
-  res.send(user);
+
+  res.send(_.pick(user, ['_id', 'name', 'email']));
 
 });
 
