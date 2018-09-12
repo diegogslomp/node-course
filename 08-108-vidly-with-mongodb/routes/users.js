3c09a98f0c88e5a0a85c10fea423a53585ae75fe
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-09-12 14:54:00 -0300

    sec 10 lec 138

--- 08-108-vidly-with-mongodb/routes/users.js
+++ 08-108-vidly-with-mongodb/routes/users.js
@@ -5,7 +5,12 @@ const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', auth, async (req, res) => {
+router.get('/me', auth, async(req, res) => {
+  const user = await User.findById(req.user._id).select('-password'); 
+  res.send(user);
+});
+
+router.post('/', async (req, res) => {
 
   const { error } = validate(req.body);
   if(error) return res.status(400).send(error.details[0].message);
