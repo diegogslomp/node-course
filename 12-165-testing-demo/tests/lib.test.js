f0ea1db5bc969e4c448b4ba4193d46eaa8b84861
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-09-17 09:29:47 -0300

    sec 12 lec 172

--- 12-165-testing-demo/tests/lib.test.js
+++ 12-165-testing-demo/tests/lib.test.js
@@ -59,3 +59,18 @@ describe('getProduct', () => {
     expect(result).toHaveProperty('id', 1);
   });
 });
+
+describe('registerUser', () => {
+  it('should throw if username is falsy', () => {
+    const args = [null, undefined, NaN, '', 0, false];
+    args.forEach(a => { expect(() => { 
+      lib.registerUser(a) }).toThrow(); 
+    });
+  });
+
+  it('should return a user object if valid username is passed', () => {
+    const result = lib.registerUser('diego');
+    expect(result).toMatchObject({ username: 'diego' });
+    expect(result.id).toBeGreaterThan(0);
+  });
+});
