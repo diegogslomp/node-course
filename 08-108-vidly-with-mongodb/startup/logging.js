482e6be28a7602409c43ead4ab554c4f35770e4c
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-09-17 11:49:56 -0300

    sec 13 lec 182

--- 08-108-vidly-with-mongodb/startup/logging.js
+++ 08-108-vidly-with-mongodb/startup/logging.js
@@ -1,6 +1,6 @@
 const winston = require('winston');
 require('express-async-errors');
-require('winston-mongodb');
+// require('winston-mongodb');
 
 module.exports = function() {
   winston.handleExceptions(
@@ -12,8 +12,8 @@ module.exports = function() {
   });
   
   winston.add(winston.transports.File, {filename: 'logfile.log'});
-  winston.add(winston.transports.MongoDB, { 
-    db: 'mongodb://localhost/vidly',
-    level: 'info' 
-  });  
+  // winston.add(winston.transports.MongoDB, { 
+  //   db: 'mongodb://localhost/vidly',
+  //   level: 'info' 
+  // });  
 }
