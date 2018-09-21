6b1a51b0127a1a15a646a7b47c165754f0a18348
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-09-21 09:10:12 -0300

    sec 14 lec 208

--- 08-108-vidly-with-mongodb/tests/integration/returns.test.js
+++ 08-108-vidly-with-mongodb/tests/integration/returns.test.js
@@ -80,4 +80,11 @@ describe('/api/returns', () => {
     expect(res.status).toBe(200);
   });
 
+  it('should set returnDate if input is valid', async () => {
+    const res = await exec();
+    const rentalInDb = await Rental.findById(rental._id);
+    const diff = new Date() - rentalInDb.dateReturned;
+    expect(diff).toBeLessThan(10 * 1000);
+  });
+
 });
