0ffcb316c512a76ea57c4befdc39033c11b1d883
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-09-21 08:40:02 -0300

    sec 14 lec 203

--- 08-108-vidly-with-mongodb/tests/integration/returns.test.js
+++ 08-108-vidly-with-mongodb/tests/integration/returns.test.js
@@ -1,4 +1,5 @@
 const { Rental } = require('../../models/rental');
+const { User } = require('../../models/user');
 const mongoose = require('mongoose');
 const request = require('supertest');
 
@@ -38,6 +39,27 @@ describe('/api/returns', () => {
     const res = await request(server)
       .post('/api/returns')
       .send({ customerId, movieId });
+
     expect(res.status).toBe(401);
+  });
+
+  it('should return 400 if customerId is not provided', async () => {
+    const token = new User().generateAuthToken();
+    const res = await request(server)
+      .post('/api/returns')
+      .set('x-auth-token', token)
+      .send({ movieId });
+
+    expect(res.status).toBe(400);
+  });
+  
+  it('should return 400 if movieId is not provided', async () => {
+    const token = new User().generateAuthToken();
+    const res = await request(server)
+      .post('/api/returns')
+      .set('x-auth-token', token)
+      .send({ customerId });
+
+    expect(res.status).toBe(400);
   });
 });
